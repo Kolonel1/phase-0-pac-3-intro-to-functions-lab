d21d5e02bd42b6eaeb152b51cb13639431eecb8b
Kolonel1 <victor.munyao@student.moringaschool.com>
2022-08-14 02:47:08 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string){return string.toUpperCase();}
+shout('hello')
+function whisper(string){return string.toLowerCase();}
+whisper('HELLO')
+function logShout(string){console.log(string.toUpperCase());}
+logShout('hello')
+function logWhisper(string){console.log(string.toLowerCase());}
+logWhisper('HELLO')
+
+
+//i am writing down an example of code 
+const string= 'hello'
+console.log(string);
+console.log(string.toUpperCase());
+console.log(string===string.toUpperCase);
+
+//it seems i need to read this if content because i do not remember shit now do i 
+//really bad news indeed because i have a lot to do 
+//and the laptop is starting to have issues here and there
+//i have not completed this work do not forget that
+
+function sayHiToHeadphonedRoommate(string){
+    if (string === string.toLowerCase()){return ('I can\'t hear you!')}
+    else if (string ===string.toUpperCase()){return('YES INDEED!')}
+    else if (string==="Let's have dinner together!"){return('I would love to!')}}
